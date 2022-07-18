18cb1d3e6502324713e81026457bc419e55ae76e
Madhuri024 <rekhamadhuri95@gmail.com>
2022-07-19 00:41:42 +0530

    tag

---
+++ dom.js
@@ -0,0 +1,12 @@
+
+
+var li= document.getElementsByTagName('li');
+li[4].style.backgroundColor ='blue';
+li[4].style.fontWeight ='bold';
+for(var i=0;i<li.length;i++){
+    li[i].style.backgroundColor='#008000';
+}
+
+//var items =document.getElementsByClassName('list-group-item');
+//items[4].textContent='HELLO';
+//items[4].style.fontWeight='bold';
